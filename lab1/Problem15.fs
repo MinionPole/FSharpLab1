c4a167845a0ebd28e3b20b7663f25b0bb0a022eb
Roman <roma.yunusov.2001@mail.ru>
2024-10-15 16:20:51 +0300

    update tests

--- lab1/Problem15.fs
+++ lab1/Problem15.fs
@@ -41,10 +41,9 @@ let factorialTailRec (n: bigint) : bigint =
 let numerator = Seq.initInfinite (fun i -> factorial (bigint (i) * 2I))
 let denominator = Seq.initInfinite (bigint >> factorial)
 
-let getAnsFromLazyCollections =
-    let x = 20
+let getAnsFromLazyCollections (x: int) =
     let numeratorOb = numerator |> Seq.take (x + 1) |> Seq.toList
     let denominatorOb = denominator |> Seq.take (x + 1) |> Seq.toList
     string (numeratorOb.[x] / denominatorOb.[x] / denominatorOb.[x])
 
-let solutions = [ solveWithFor ]
+let solutions = [ getAnsFromLazyCollections ]
