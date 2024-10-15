3c7f9154b640b6e3061d2ac5bd9d9f6607e244f7
Roman <roma.yunusov.2001@mail.ru>
2024-10-15 14:56:27 +0300

    change tests

--- lab1_tests/Tests.fs
+++ lab1_tests/Tests.fs
@@ -9,13 +9,13 @@ let ``My test`` () = Assert.True(true)
 [<Fact>]
 let ``Check Problem 15`` () =
     Problem15.solutions
-    |> List.iter (fun solution ->
-        let res: bigint = solution ()
+    |> List.iter (fun res ->
         Assert.Equal(res, 137846528820I))
+
 
 [<Fact>]
 let ``Check Problem 16`` () =
     Problem16.solutions
-    |> List.iter (fun solution ->
-        let res: int = solution ()
+    |> List.iter (fun res ->
         Assert.Equal(res, 1366))
+
