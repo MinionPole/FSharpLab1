c7d231330ba11f1daa0e9ceb3e99f1b60a6b945f
Roman <roma.yunusov.2001@mail.ru>
2024-10-15 15:18:41 +0300

    update tests

--- lab1_tests/Tests.fs
+++ lab1_tests/Tests.fs
@@ -8,9 +8,15 @@ let ``My test`` () = Assert.True(true)
 
 [<Fact>]
 let ``Check Problem 15`` () =
-    Problem15.solutions |> List.iter (fun res -> Assert.Equal(res, "137846528820"))
+    Problem15.solutions
+    |> List.iter (fun solution ->
+        let actual = solution () // вызываем решение, чтобы получить результат
+        Assert.Equal("137846528820", actual))
 
 
 [<Fact>]
 let ``Check Problem 16`` () =
-    Problem16.solutions |> List.iter (fun res -> Assert.Equal(res, "1366"))
+    Problem16.solutions
+    |> List.iter (fun solution ->
+        let actual = solution () // вызываем решение, чтобы получить результат
+        Assert.Equal("1366", actual))
