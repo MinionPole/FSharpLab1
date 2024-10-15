7e560c68932b400d2b07e38e7f6def96ee21d756
Roman <roma.yunusov.2001@mail.ru>
2024-10-15 15:45:49 +0300

    update tests

--- lab1_tests/Tests.fs
+++ lab1_tests/Tests.fs
@@ -5,3 +5,10 @@ open Xunit
 
 [<Fact>]
 let ``My test`` () = Assert.True(true)
+
+[<Fact>]
+let ``Check Problem 16`` () =
+    Problem16.solutions
+    |> List.iter (fun solution ->
+        let actual = solution 1000 // вызываем решение, чтобы получить результат
+        Assert.Equal("1366", actual))
